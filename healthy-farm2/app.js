39504756a6d880fb00c9079b8943d8e4ef538a0c
dactue1006 <dactue1006@gmail.com>
2018-12-18 20:32:37 +0700

    view products

--- healthy-farm2/app.js
+++ healthy-farm2/app.js
@@ -1,13 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/user.route');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/user.route');
+const productsRouter = require('./routes/product.route');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,12 +22,28 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// dotenv config
+dotenv.config();
+
+// connect to database
+mongoose.connect(
+  process.env.URI_LOCAL,{
+    useNewUrlParser: true
+  }
+).then((result)=> {
+  console.log('connect to database successfully');
+}).catch((err)=>{
+  console.log(err);
+  process.exit();
+})
+
 app.use((req, res, next)=> {
   res.locals.login = true;
   next();
 })
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/products', productsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

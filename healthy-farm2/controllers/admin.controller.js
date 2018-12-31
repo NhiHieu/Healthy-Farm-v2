8ccc5201fc601e0a2f179996e9d29e06b6ffb203
dactue1006 <dactue1006@gmail.com>
2018-12-31 23:03:23 +0700

    add admin

---
+++ healthy-farm2/controllers/admin.controller.js
@@ -0,0 +1,106 @@
+const Role = require('../models/role.model');
+const Product = require('../models/product.model');
+const Category = require('../models/category.model');
+
+const isAuthAdmin = (req, res, next) => {
+  const id = req.user.role;
+  Role.findById(id, (err, role)=> {
+    if (err) {
+      next(err);
+    }
+    if (!role || role.key=='USER') {
+      res.redirect('/');
+    } else {
+      next();
+    }    
+  })
+
+}
+
+/* manage product */
+const getProducts = (req, res, next) => {
+  Product
+    .find()
+    .populate('category')
+    .exec((err, products)=> {
+      if (err) {
+        res.redirect('/admin');
+      }
+      Category.find({}, (error, categories)=> {
+        res.render('admin/products', {
+          products,
+          categories
+        })
+      })
+      
+    })
+}
+
+const postProduct = (req, res, next) => {
+  // name, imgUrl, description, price, unit, category
+  let product = new Product({
+    ...req.params
+  });
+
+  product.save((err, product)=> {
+    if (err) {
+      res.json({
+        message: 'Failure'
+      })
+    }
+    res.json({
+      message: 'Success',
+      product
+    })
+  })
+
+}
+
+const editProduct = (req, res, next) => {
+  const id = req.params.productId;
+  const updateOps = {};
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value;
+  }
+  Product
+    .updateOne({ _id: id}, { $set: updateOps })
+    .exec((err, product)=> {
+      if (err) {
+        res.json({
+          message: 'Failure'
+        })
+      }
+      res.json({
+        message: 'Success'
+      })
+    })
+    
+}
+
+const deleteProduct = (req, res, next) => {
+  const id = req.params.productId;
+  Product
+    .deleteOne({ _id: id})
+    .exec((err, res)=> {
+      if (err) {
+        res.json({
+          message: "Failure"
+        })
+      }
+      res.json({
+        message: "Success"
+      })
+    })
+}
+/* manage product */
+
+module.exports = {
+  // authentication admin
+  isAuthAdmin,
+  // product
+  getProducts,
+  postProduct,
+  editProduct,
+  deleteProduct
+  // user
+}
